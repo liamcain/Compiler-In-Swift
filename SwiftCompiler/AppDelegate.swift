cfe8027b1debd7da570c19d32d38038e1d7f53ba
Liam Cain <contactliam529@gmail.com>
2015-01-29 12:33:05 -0500

    Initial Commit

---
+++ SwiftCompiler/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftCompiler
+//
+//  Created by Liam Cain on 1/29/15.
+//  Copyright (c) 2015 Liam Cain. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
